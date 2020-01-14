7db35f25af18984c3bbd138275ebd3599bcba5c5
Bruno Augusto Constantino <bconst123@gmail.com>
2020-01-14 20:44:00 -0300

    Initial Commit

---
+++ MarvelApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelApplication
+//
+//  Created by Bruno Augusto Constantino on 1/14/20.
+//  Copyright © 2020 Bruno Augusto Constantino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
